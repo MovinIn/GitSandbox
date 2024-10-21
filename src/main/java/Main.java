7d3438a510c3e18773ce08c8cf41d755053cf527
la20jose <lamj5861@lgsstudent.org>
2024-10-21 18:57:23 -0500

    added to see branches change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[]args) {
     //TODO: the comment we will test merge conflicts on! this comment was modified in intellij
-    // asd
+    // asd 12312345
   }
 }
